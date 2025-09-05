7975f736ef6c791f3f837c0d8f9143bfd4de91a7
Win Min <winminthuzar1@gmail.com>
2025-09-05 22:08:13 +0800

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'wmk-calc-v1';
+const ASSETS = ['/', '/index.html', '/manifest.webmanifest'];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
