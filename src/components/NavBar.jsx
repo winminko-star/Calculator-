823693586830a62e2310337301517c1679fdac02
Win Min <winminthuzar1@gmail.com>
2025-09-06 19:50:50 +0800

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import { NavLink, useNavigate } from 'react-router-dom'
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
-export default function NavBar({ user, onLogout }){
-  const navigate = useNavigate()
+export default function NavBar({ user, onLogout }) {
+  const navigate = useNavigate();
+
   return (
     <div className="header">
       <div className="container">
-        <div className="row" style={{justifyContent:'space-between'}}>
+        <div className="row" style={{ justifyContent: "space-between" }}>
           <strong>WMK Calc</strong>
           <nav className="nav">
-            <NavLink to="/" end>Home</NavLink>
+            <NavLink to="/" end>
+              Home
+            </NavLink>
             <NavLink to="/drawing2d">2D Drawing</NavLink>
             <NavLink to="/review">All Review</NavLink>
+            <NavLink to="/righttriangle">Right Triangle</NavLink>
+            <NavLink to="/circlecenter">Circle Center</NavLink>
+            <NavLink to="/levelling">Levelling</NavLink>
+
             {user ? (
-              <button className="btn" onClick={async ()=>{ await onLogout?.(); navigate('/login', { replace:true }) }}>Logout</button>
+              <button
+                className="btn"
+                type="button"
+                onClick={async () => {
+                  try {
+                    await onLogout?.();
+                  } finally {
+                    navigate("/login", { replace: true });
+                  }
+                }}
+                style={{ marginLeft: 8 }}
+              >
+                Logout
+              </button>
             ) : (
               <NavLink to="/login">Login</NavLink>
             )}
@@ -21,25 +41,5 @@ export default function NavBar({ user, onLogout }){
         </div>
       </div>
     </div>
-  )
-}
-```jsx
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-
-export default function NavBar(){
-  return (
-    <div className="header">
-      <div className="container">
-        <div className="row" style={{justifyContent:'space-between'}}>
-          <strong>WMK Calc</strong>
-          <nav className="nav">
-            <NavLink to="/" end>Home</NavLink>
-            <NavLink to="/drawing2d">2D Drawing</NavLink>
-            <NavLink to="/review">All Review</NavLink>
-          </nav>
-        </div>
-      </div>
-    </div>
-  )
+  );
 }
