bf3748f8a9564563756e78c6bbc3912d27b939a0
Win Min <winminthuzar1@gmail.com>
2025-09-06 18:55:50 +0800

    Create Levelling.jsx

---
+++ src/pages/Levelling.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Levelling() {
+  return (
+    <div className="card">
+      <div className="page-title">📏 Levelling Calculator</div>
+      <p>Coming soon…</p>
+    </div>
+  );
+}
