5af40b8a38bb7b5e57f3f72317013095352b7102
Win Min <winminthuzar1@gmail.com>
2025-09-06 18:53:51 +0800

    Create RightTriangle.jsx

---
+++ src/pages/RightTriangle.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function RightTriangle() {
+  return (
+    <div className="card">
+      <div className="page-title">📐 Right Triangle Calculator</div>
+      <p>Coming soon…</p>
+    </div>
+  );
+}
