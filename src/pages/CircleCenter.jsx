7756567ec42dda93e09602aa6e8c78780b83fd4c
Win Min <winminthuzar1@gmail.com>
2025-09-06 18:55:03 +0800

    Create CircleCenter.jsx

---
+++ src/pages/CircleCenter.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CircleCenter() {
+  return (
+    <div className="card">
+      <div className="page-title">⭕ Circle Center Calculator</div>
+      <p>Coming soon…</p>
+    </div>
+  );
+}
