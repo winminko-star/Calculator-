843e0330062769b0e2a7e722d8eade3bc291292a
Win Min <winminthuzar1@gmail.com>
2025-10-14 22:03:00 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+import Dashboard from "./pages/Dashboard";
+import Review from "./pages/Review";
 import ErrorBoundary from "./ErrorBoundary";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/review" element={<Review />} />
+        </Routes>
+      </BrowserRouter>
     </ErrorBoundary>
   </React.StrictMode>
 );
