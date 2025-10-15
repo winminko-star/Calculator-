4b35f67f0f2dae1e0c4b99886306e1ebf83821af
Win Min <winminthuzar1@gmail.com>
2025-10-15 10:03:24 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import ErrorBoundary from "./ErrorBoundary";
 
-createRoot(document.getElementById('root')).render(
-<BrowserRouter>
+createRoot(document.getElementById("root")).render(
+<React.StrictMode>
+<ErrorBoundary>
 <App />
-</BrowserRouter>
-)
-
+</ErrorBoundary>
+</React.StrictMode>
+);
