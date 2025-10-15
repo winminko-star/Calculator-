2db9cebdacf49e21710953bfcbc5643c968c9272
Win Min <winminthuzar1@gmail.com>
2025-10-15 08:38:10 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
-// src/main.jsx or src/index.jsx
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+createRoot(document.getElementById('root')).render(
+<BrowserRouter>
+<App />
+</BrowserRouter>
 )
+
