1f5ebc28f7f6c161e008e4f03ede05752894c14c
Win Min <winminthuzar1@gmail.com>
2025-10-17 15:30:24 +0800

    Refactor root rendering in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
+// src/main.jsx (or src/index.js)
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import ErrorBoundary from "./ErrorBoundary";
 
-createRoot(document.getElementById("root")).render(
-<React.StrictMode>
-<ErrorBoundary>
-<App />
-</ErrorBoundary>
-</React.StrictMode>
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
 );
