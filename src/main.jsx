e285a2ef7f2037885862f5585c7b38a9561ac5b5
Win Min <winminthuzar1@gmail.com>
2025-10-17 14:58:11 +0800

    Refactor rendering structure in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,9 @@ import "./index.css";
 import ErrorBoundary from "./ErrorBoundary";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
+<React.StrictMode>
+<ErrorBoundary>
+<App />
+</ErrorBoundary>
+</React.StrictMode>
 );
-
-// ⏳ Optional: Hide splash after 4 seconds
-setTimeout(() => {
-  const splash = document.getElementById("splash-screen");
-  if (splash) splash.style.display = "none";
-}, 4000);
