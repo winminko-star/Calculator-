5b57c3f4674b3f6bb0164f8da7fc98541b26973e
Win Min <winminthuzar1@gmail.com>
2025-10-19 16:17:32 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['file-saver'], // ✅ external file error မဖြစ်အောင် ထည့်
-    },
-  },
+  base: './', // Netlify, GitHub Pages မှာသုံးရင်အရေးကြီး
 })
